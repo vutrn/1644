ef54b4437691a810064c6525cf2d669b98403638
vutrn <meoiuwai@gmail.com>
2023-07-13 13:10:30 +0700

    nodejs

--- lab01/demo.js
+++ lab01/demo.js
@@ -2,6 +2,23 @@ console.log('g');
 console.log('aasd');
 
 
-a = 3
-console.log(typeof(a))
+// console.log(typeof(a))
+
+for(i = 0; i < 10; i++){
+    console.log(i)
+}
+
+function World() {
+  console.log('Hello');
+}
+
+World();
+
+let a = 5
+function sum() {
+  let a = 2
+  console.log(a);
+}
+sum(2, 3);
+
 
