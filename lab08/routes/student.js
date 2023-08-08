7af9554a0aba6fdb99ed6b8eb1fde5624f3e9fe0
vutrn <meoiuwai@gmail.com>
2023-08-08 15:51:44 +0700

    ok

--- lab08/routes/student.js
+++ lab08/routes/student.js
@@ -9,4 +9,29 @@ router.get("/", async (req, res) => {
   res.render("student/studentList", { student });
 });
 
+router.get("/add", (req, res) => {
+  res.render("student/studentAdd");
+});
+
+router.post("/add", async (req, res) => {
+  await studentModel.create(req.body);
+  res.redirect("/student");
+});
+
+router.get("/edit/:id", async (req, res) => {
+  const student = await studentModel.findById(req.params.id);
+  res.render("student/studentEdit", { student });
+  // res.redirect("/student");
+});
+
+router.post("/edit/:id", async (req, res) => {
+  await studentModel.findByIdAndUpdate(req.params.id, req.body)
+  res.redirect("/student");
+});
+
+router.get("/delete/:id", async (req, res) => {
+  await studentModel.findByIdAndDelete(req.params.id);
+  res.redirect("/student");
+});
+
 module.exports = router;
